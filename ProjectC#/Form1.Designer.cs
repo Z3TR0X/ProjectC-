41583156a8dd541fbea69c14a23d79f5b8d0bb81
eporc <eporc@VANOUPC>
2026-01-26 19:14:33 +0100

    Ajoutez des fichiers projet.

---
+++ ProjectC#/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ProjectC_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button_minimize = new System.Windows.Forms.Button();
+            this.button_maximize = new System.Windows.Forms.Button();
+            this.button_close = new System.Windows.Forms.Button();
+            this.logo = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
+            this.panel1.Controls.Add(this.logo);
+            this.panel1.Controls.Add(this.button_minimize);
+            this.panel1.Controls.Add(this.button_maximize);
+            this.panel1.Controls.Add(this.button_close);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1399, 34);
+            this.panel1.TabIndex = 0;
+            this.panel1.Click += new System.EventHandler(this.panel1_Click);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            // 
+            // button_minimize
+            // 
+            this.button_minimize.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
+            this.button_minimize.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button_minimize.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
+            this.button_minimize.FlatAppearance.BorderSize = 0;
+            this.button_minimize.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(63)))), ((int)(((byte)(69)))));
+            this.button_minimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_minimize.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
+            this.button_minimize.Image = global::ProjectC_.Properties.Resources.Minimize_white;
+            this.button_minimize.Location = new System.Drawing.Point(1270, 0);
+            this.button_minimize.Margin = new System.Windows.Forms.Padding(0);
+            this.button_minimize.Name = "button_minimize";
+            this.button_minimize.Size = new System.Drawing.Size(43, 34);
+            this.button_minimize.TabIndex = 3;
+            this.button_minimize.UseVisualStyleBackColor = false;
+            this.button_minimize.Click += new System.EventHandler(this.button_minimize_Click);
+            // 
+            // button_maximize
+            // 
+            this.button_maximize.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
+            this.button_maximize.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button_maximize.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
+            this.button_maximize.FlatAppearance.BorderSize = 0;
+            this.button_maximize.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(63)))), ((int)(((byte)(69)))));
+            this.button_maximize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_maximize.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
+            this.button_maximize.Image = global::ProjectC_.Properties.Resources.Maximize_White;
+            this.button_maximize.Location = new System.Drawing.Point(1313, 0);
+            this.button_maximize.Margin = new System.Windows.Forms.Padding(0);
+            this.button_maximize.Name = "button_maximize";
+            this.button_maximize.Size = new System.Drawing.Size(43, 34);
+            this.button_maximize.TabIndex = 2;
+            this.button_maximize.UseVisualStyleBackColor = false;
+            this.button_maximize.Click += new System.EventHandler(this.button_maximize_Click);
+            // 
+            // button_close
+            // 
+            this.button_close.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
+            this.button_close.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button_close.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
+            this.button_close.FlatAppearance.BorderSize = 0;
+            this.button_close.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(0)))), ((int)(((byte)(40)))));
+            this.button_close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_close.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
+            this.button_close.Image = ((System.Drawing.Image)(resources.GetObject("button_close.Image")));
+            this.button_close.Location = new System.Drawing.Point(1356, 0);
+            this.button_close.Margin = new System.Windows.Forms.Padding(0);
+            this.button_close.Name = "button_close";
+            this.button_close.Size = new System.Drawing.Size(43, 34);
+            this.button_close.TabIndex = 1;
+            this.button_close.UseVisualStyleBackColor = false;
+            this.button_close.Click += new System.EventHandler(this.button_close_Click);
+            // 
+            // logo
+            // 
+            this.logo.Image = global::ProjectC_.Properties.Resources.IconDarkMode;
+            this.logo.Location = new System.Drawing.Point(0, 0);
+            this.logo.Name = "logo";
+            this.logo.Size = new System.Drawing.Size(34, 34);
+            this.logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.logo.TabIndex = 1;
+            this.logo.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1399, 852);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Resize += new System.EventHandler(this.Form1_SizeChanged);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button_close;
+        private System.Windows.Forms.Button button_minimize;
+        private System.Windows.Forms.Button button_maximize;
+        private System.Windows.Forms.PictureBox logo;
+    }
+}
+
---
+++ ProjectC#/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectC_
+{
+    public partial class Form1 : Form
+    {
+
+        Size last_normal_form_size;
+        Point last_normal_form_position;
+
+        //Fonction pour ajouter l'ombre autour de la fenêtre
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ClassStyle |= 0x20000; // Ombre portée
+                return cp;
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if (m.Msg == 0x84)
+            {
+                Point screenPoint = new Point(m.LParam.ToInt32());
+                Point clientPoint = this.PointToClient(screenPoint);
+
+                //Si la souris est dans le coin inférieur gauche dans une zone de 16 pixels (pour la marge)
+                if (clientPoint.X >= this.Size.Width - 16 && clientPoint.Y >= this.Size.Height - 16)
+                {
+                    m.Result = (IntPtr)17;
+                    return;
+                }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button_maximize_Click(object sender, EventArgs e)
+        {
+            Size screen_size = Screen.FromControl(this).WorkingArea.Size;
+            if (this.Size != screen_size)
+            {
+                last_normal_form_size = this.Size;
+                last_normal_form_position = this.Location;
+                this.Size = Screen.FromControl(this).WorkingArea.Size;
+                this.Location = new Point(0, 0);
+                button_maximize.Image = Properties.Resources.Restore_white;
+            }
+            else
+            {
+                this.Size = last_normal_form_size;
+                this.Location = last_normal_form_position;
+                button_maximize.Image = Properties.Resources.Maximize_White;
+            }
+
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            int header_height = 34;
+            int header_width = this.Size.Width;
+            Size button_size = new Size(34, header_height);
+            panel1.Size = new Size(header_width, header_height);
+            button_close.Size = button_size;
+            button_maximize.Size = button_size;
+            button_minimize.Size = button_size;
+            logo.Size = new Size(34,34);
+        }
+
+        private void button_minimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (this.Size == Screen.FromControl(this).WorkingArea.Size)
+            {
+                this.Size = last_normal_form_size;
+                double ratio = (double)e.X / (double)Screen.GetWorkingArea(this).Size.Width;
+                Point cursorPoint = Cursor.Position;
+                int newX = cursorPoint.X - (int)(this.Width * ratio);
+                int newY = cursorPoint.Y - e.Y;
+
+                this.Location = new Point(newX, newY);
+
+                button_maximize.Image = Properties.Resources.Maximize_White;
+            }
+            
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            this.Size = last_normal_form_size;
+            this.Location = last_normal_form_position;
+            button_maximize.Image = Properties.Resources.Maximize_White;
+        }
+    }
+}
+
+
+
